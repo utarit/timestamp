defa7401b88935ffb5938856532de78bff5d90dd
Glitch (timestamp-akca) <none>
2018-06-01 12:16:58 +0000

    🐯🏨 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+
+app.get("/api/timestamp/", function (req, res) {
+  const time = new Date()
+  
+  res.json({"unix": time.getTime(), "utc": time.toUTCString()})
+});
+
+app.get("/api/timestamp/:time", function (req, res) {
+  const time = new Date(req.params.time)
+  const date = time.toUTCString()
+  const unix = time.getTime()
+  res.json({"unix": unix, "utc": date})
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
